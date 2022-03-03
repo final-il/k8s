5a22a325b52b4a22d2f448405646347f7e776bb3
Maor B <maorb@final.co.il>
2022-03-03 14:48:36 +0000

    move to n.verginia

--- tf/main.tf
+++ tf/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "master_node" {
-  ami           = "ami-0848c3f903457a484"
+  ami           = "ami-0b61977be58df3188"
   instance_type = "t2.micro"
   count         = 3
-  subnet_id = "subnet-01df9070f1a32d9e9"
+  subnet_id = "subnet-06ae071fa4a2ed7b7"
   associate_public_ip_address = "${count.index == 0 ? "true" : 0 }"
-  security_groups = ["sg-052ef06a60df8ecc0"]
+  security_groups = ["sg-02dc9c1a4e7b1bf5a"]
   tags = {
     Name = "master-node-${count.index + 1}"
     uid = "k8s"
@@ -12,12 +12,12 @@ resource "aws_instance" "master_node" {
 }
 
 resource "aws_instance" "worker_node" {
-  ami           = "ami-0848c3f903457a484"
+  ami           = "ami-0b61977be58df3188"
   instance_type = "t2.micro"
   count         = 2
-  subnet_id = "subnet-01df9070f1a32d9e9"
+  subnet_id = "subnet-06ae071fa4a2ed7b7"
   associate_public_ip_address = "false"
-  security_groups = ["sg-052ef06a60df8ecc0"]
+  security_groups = ["sg-02dc9c1a4e7b1bf5a"]
 
   tags = {
     Name = "worker-node-${count.index + 1}"
